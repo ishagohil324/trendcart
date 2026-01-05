f53c2bfb467801bdfba2b99c8f7d3618368c63ca
ISHA GOHIL <ishagohil324@gmail.com>
2026-01-05 11:50:12 +0530

    daily commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+// import { initializeApp } from "firebase/app";
 
 // YOUR Firebase configuration (from Step 4)
 const firebaseConfig = {
@@ -23,3 +24,30 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
+
+
+
+
+
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCMT2IsM5Puft1ymokyP1-mt0UhsoxpDz8",
+//   authDomain: "trendcart-3c4ee.firebaseapp.com",
+//   projectId: "trendcart-3c4ee",
+//   storageBucket: "trendcart-3c4ee.firebasestorage.app",
+//   messagingSenderId: "454131361688",
+//   appId: "1:454131361688:web:0aff362fd553138ebb2ded"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+// export default app;
